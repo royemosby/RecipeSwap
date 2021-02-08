a9678df7c1922301e91b049a92d9404105e3bc77
Roy Mosby <ego.madking@gmail.com>
2021-02-08 11:04:41 -0500

    create recipe table migration

---
+++ db/migrate/20210208135653_create_recipe.rb
@@ -0,0 +1,14 @@
+class CreateRecipe < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string    :name
+      t.string    :description
+      t.string    :ingredients
+      t.string    :instructions
+      t.integer   :servings
+      t.integer   :user_id #author
+      t.datetime  :created_at
+      t.datetime  :updated_at
+    end
+  end
+end
