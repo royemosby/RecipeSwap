95f4e8959bb96657f900929e2489cf552358c482
Roy Mosby <ego.madking@gmail.com>
2021-02-09 17:42:24 -0500

    added user session helpers, check if logged in on homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,20 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    if logged_in?
+      @user = current_user
+    end
     erb :index
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
 
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 end
