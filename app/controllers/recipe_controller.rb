d12074e18de7f5564206623752a36bc8a7e18bfe
Roy Mosby <ego.madking@gmail.com>
2021-02-09 12:44:10 -0500

    created view routes for recipes and each recipe

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,3 +1,12 @@
 class RecipeController < ApplicationController
   
+  get "/recipes" do
+    @recipes = Recipe.all
+      erb :"recipes/index"
+  end
+
+  get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    erb :"recipes/show"
+  end
 end
