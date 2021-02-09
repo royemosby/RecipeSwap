5b63e6a88797b3f7199e1480662ad1eba341c67f
Roy Mosby <ego.madking@gmail.com>
2021-02-09 13:11:38 -0500

    cleaned quotes, added user/new, post to users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,27 @@
 class UserController < ApplicationController
 
-get "/users" do
-  @users = User.all
-  erb :"users/index"
-end
+  get '/users' do
+    @users = User.all
+    erb :'users/index'
+  end
 
-get "/users/:id" do
-  @user = User.find(params[:id])
-  erb :"users/show"
-end
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    erb :'users/show'
+  end
+
+  post '/users' do
+    if !!params[:username] && !!params[:password]
+      user = User.create(:username => params[:username], :password => params[password], :email => params[:email])
+      redirect to "/users/#{user.id}"
+    else
+      redirect to '/users/error' #TODO: create error route
+    end
+  end
+
 
 end
