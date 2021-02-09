fbe2673d197fdbcaace571bda7d56164b9582e68
Roy Mosby <ego.madking@gmail.com>
2021-02-09 13:43:30 -0500

    added get users/#/edit, sterted post /users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -18,14 +18,26 @@ class UserController < ApplicationController
     erb :'users/show'
   end
 
+  get '/users/:id/edit' do
+    #TODO: ensure current user or route to unauthorized
+    @user = User.find(params[:id])
+    erb :'users/edit'
+  end
+
+  patch '/users' do
+    #TODO: validate against session and params[:old_password]
+    #TODO: update user. Include new_password IF NOT empty
+  end
+
   post '/users' do
     if !!params[:username] && !!params[:password]
-      user = User.create(:username => params[:username], :password => params[password], :email => params[:email])
+      user = User.create(:username => params[:username], :password => params[:password], :email => params[:email])
       redirect to "/users/#{user.id}"
     else
-      redirect to '/users/error' #TODO: create error route
+      redirect to '/users/error'
     end
   end
 
+  #TODO: add session helpers
 
 end
