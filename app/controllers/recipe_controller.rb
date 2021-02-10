9165a94850b6bf48e859bf1670b5ba7f9144d45d
Roy Mosby <ego.madking@gmail.com>
2021-02-10 11:58:15 -0500

    add new recipe route

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -7,6 +7,15 @@ class RecipeController < ApplicationController
     @recipes = Recipe.all
       erb :"recipes/index"
   end
+  
+    get '/recipes/new' do
+    if logged_in?
+      erb :'recipes/new'
+    else
+      flash[:message] = "You have to be logged in to create a recipe."
+      erb :'recipes/error'
+    end
+  end
 
   get "/recipes/:id" do
     @recipe = Recipe.find(params[:id])
