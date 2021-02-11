22eabbe24d6561c9515871ac984187fa5edd0501
Roy Mosby <ego.madking@gmail.com>
2021-02-11 12:33:09 -0500

    associated Recipe as "favorite to User as "collector" through collector_favorites table

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   has_many :spinoffs, class_name: "Recipe", foreign_key: "original_id"
   belongs_to :original, class_name: "Recipe"
+  has_many :collector_favorites, foreign_key: "favorite_id"
+  has_many :collectors, class_name: "User", through: :collector_favorites
 end
