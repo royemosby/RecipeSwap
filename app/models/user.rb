98abd32c208d4f294ec98e24e79251323158aa6b
Roy Mosby <ego.madking@gmail.com>
2021-02-11 12:33:49 -0500

    associated User as "collector" to Recipe as "favorite" through collector_favorites table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User <ActiveRecord::Base
   has_many :recipes
+  has_many :collector_favorites, foreign_key: "collector_id"
+  has_many :favorites, class_name: "Recipe", through: :collector_favorites
   has_secure_password
 end
