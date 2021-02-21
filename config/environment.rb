577b2046fea0e03aa18af2894db1ec55a3f447e4
Roy Mosby <ego.madking@gmail.com>
2021-02-20 23:45:56 -0500

    trying something else to deal with database adapter

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,20 @@
-ENV['SINATRA_ENV'] ||= "development"
+# ENV['SINATRA_ENV'] ||= "development"
 
+# require 'bundler/setup'
+# Bundler.require(:default, ENV['SINATRA_ENV'])
+
+# set :database_file, "./database.yml"
+
+# require_all 'app'
+
+
+ENV['RACK_ENV'] ||= "development"
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
-set :database_file, "./database.yml"
-
+Bundler.require(:default, ENV['RACK_ENV'])
+configure :development do
+    set :database, 'sqlite3:db/development.sqlite'
+end
+configure :production do
+    set :database, ENV['DATABASE_URL']
+end
 require_all 'app'
