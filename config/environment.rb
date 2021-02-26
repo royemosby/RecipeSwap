65d76554ad1b73a391781b6db728294dc5503045
Roy Mosby <ego.madking@gmail.com>
2021-02-26 12:21:34 -0500

    revert to sqlite3 gem for demo

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "postgresql",
-  :database => "db/#{ENV['SINATRA_ENV']}"
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
 require './app/controllers/application_controller'
