b81835de996f05cd8270497e806d1561167b8a98
Roy Mosby <ego.madking@gmail.com>
2021-03-22 22:07:24 -0400

    removed comments, simplify requires in line with FI deploy instructions

--- config/environment.rb
+++ config/environment.rb
@@ -1,20 +1,6 @@
-# ENV['SINATRA_ENV'] ||= "development"
-
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['SINATRA_ENV'])
-
-# set :database_file, "./database.yml"
-
-# require_all 'app'
-
-
-ENV['RACK_ENV'] ||= "development"
 require 'bundler/setup'
-Bundler.require(:default, ENV['RACK_ENV'])
-configure :development do
-    set :database, 'sqlite3:db/development.sqlite'
-end
-configure :production do
-    set :database, ENV['DATABASE_URL']
-end
+Bundler.require
+require 'dotenv/load'
+require 'rack-flash'
+
 require_all 'app'
